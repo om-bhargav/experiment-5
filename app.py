6de9c8930987486c236c50735a3f6cc18f2101eb
om-bhargav <ombhargav788@gmail.com>
2026-09-20 08:17:47 +0000

    version 2 added

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
  
 @app.route("/")
-def home(): return "CI/CD Pipeline Version 1"
+def home(): return "CI/CD Pipeline Version 2"
  
 if __name__ == "__main__": app.run("0.0.0.0", 5000)
